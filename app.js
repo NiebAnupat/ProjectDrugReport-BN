71d5cd006913b0cddb6be8e1036dfd1a9e402af8
Nieb Anupat <anupat.k64@rsu.ac.th>
2023-01-07 15:53:50 +0700

    build : config for production

--- app.js
+++ app.js
@@ -13,27 +13,22 @@ require("dotenv").config();
 
 const app = express();
 const corsConfig = {
-  origin: "https://anupat-dav.com",
+  origin: "http://localhost:3000",
   credentials: true,
 };
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 global.__basedir = __dirname;
 
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// import reportRouter from "./src/router/report.js";
-// import authRouter from "./src/router/auth.js";
-// import { cors } from "cors";
-// import { path } from "path";
 const reportRouter = require("./src/router/report");
 const authRouter = require("./src/router/auth");
 app.use("/report", reportRouter);
 app.use("/auth", authRouter);
 
+// comment out the following line if you want to use the server.js file
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server is running on port ${process.env.PORT}`);
 // });
